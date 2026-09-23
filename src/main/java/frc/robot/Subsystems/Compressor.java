b8a4912aa58a14a3342a2d0f83499d03cae01040
Ishant Batra <ishantpbatra1234@gmail.com>
2026-09-22 21:59:17 -0600

    wow state machine for compressor

--- src/main/java/frc/robot/Subsystems/Compressor.java
+++ src/main/java/frc/robot/Subsystems/Compressor.java
@@ -3,15 +3,23 @@ package frc.robot.Subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Compressor extends SubsystemBase{
+    public enum CompressorStates{
+        IDLE,
+        ON
+    }
+
     VictorSPX compressor = new VictorSPX(8);
-    boolean on = true;
+    DigitalInput pressureSensor = new DigitalInput(1);
+
+    CompressorStates currentState = CompressorStates.IDLE;
 
     public Compressor(){
-        compressor.set(ControlMode.PercentOutput, 1);
+
     }
 
     public void stopCompressor(){
@@ -23,12 +31,23 @@ public class Compressor extends SubsystemBase{
     }
 
     public Command changeCompressor(){
-        return this.run(()->{if(on){stopCompressor(); on = false;}
-        if(!on){startCompressor(); on = true;}});
+        return this.runOnce(()->{
+            if(currentState == CompressorStates.IDLE) currentState = CompressorStates.ON;
+            else if(currentState == CompressorStates.ON) currentState = CompressorStates.IDLE;
+        });
     }
 
     @Override
     public void periodic(){
-        
+        switch(currentState){
+            case IDLE:
+                stopCompressor();
+                break;
+
+            case ON:
+                if(pressureSensor.get())stopCompressor();
+                else{startCompressor();}
+                break;
+        }
     }
 }
